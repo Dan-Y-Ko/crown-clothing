55b9b3a07975654e148ea76a1faf82b95583b1ca
Dan <danko56666@gmail.com>
2020-05-11 06:20:41 -0400

    lecture 95

--- src/App.js
+++ src/App.js
@@ -32,7 +32,6 @@ class App extends React.Component {
               ...snapShot.data(),
             },
           });
-          console.log(this.state);
         });
       } else {
         this.setState({ currentUser: userAuth });

2db32ae7ea0f8d18db5527f1251a884a31f4fe0c
Dan <danko56666@gmail.com>
2020-05-10 22:44:33 -0400

    lecture 69

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/hats" component={HatsPage} />
+        <Route path="/shop/hats" component={HatsPage} />
       </Switch>
     </div>
   );

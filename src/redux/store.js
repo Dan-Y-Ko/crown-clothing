79f7c8c4787d19b02608e66431d0e9f97d30925d
Dan <danko56666@gmail.com>
2020-05-11 23:57:16 -0400

    setup redux dev tools

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,22 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import logger from "redux-logger";
 
 import rootReducer from "./root-reducer";
 
+/*
+I prefer using redux dev tools (chrome extension) so 
+I am setting that up here. This is optional. If you 
+prefer redux dev tools, go ahead and copy this as well
+*/
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+//----------------------------------------------------------------------------------
+
 const middlewares = [logger];
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+// compose enhancers is optional for redux dev tools
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(...middlewares))
+);
 
 export default store;

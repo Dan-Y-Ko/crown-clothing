e460ca094922df5648de5e0f6d426fa3b49340f1
Dan <danko56666@gmail.com>
2020-05-12 02:33:23 -0400

    alternative to mapdispatchtoprops part 2

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -28,3 +28,39 @@ const mapDispatchToProps = (dispatch) => ({
 });
 
 export default connect(null, mapDispatchToProps)(CollectionItem);
+
+/* 
+My take on mapdispatchtoprops part 2. This is entirely optional, if you prefer the way Yihua does it, stick to that. Also
+I am not making any claims on which way is better, but I prefer the way I'm about to show.
+Having an explicit mapdispatchtoprops is not necessary. You can just pass in an object in the 2nd argument of connect
+with the actions that you want to use, and use it directly in the component and normal. This code works even if you 
+need to pass in a payload.
+
+---- Code below ----
+import React from "react";
+import { connect } from "react-redux";
+
+import CustomButton from "../custom-button/custom-button.component";
+import { addItem } from "../../redux/cart/cart.actions";
+
+import "./collection-item.styles.scss";
+
+const CollectionItem = ({ item, addItem }) => {
+  const { name, price, imageUrl } = item;
+
+  return (
+    <div className="collection-item">
+      <div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
+      <div className="collection-footer">
+        <span className="name">{name}</span>
+        <span className="price">{price}</span>
+      </div>
+      <CustomButton onClick={() => addItem(item)} inverted>
+        Add to Cart
+      </CustomButton>
+    </div>
+  );
+};
+
+export default connect(null, { addItem })(CollectionItem);
+*/

92b3bf672cda68a08fc30d3826a22167347b9598
Dan <danko56666@gmail.com>
2020-05-12 00:25:18 -0400

    lecture 108

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,6 +4,7 @@ import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 
 import { auth } from "../../firebase/firebase.utils";
+import CartIcon from "../cart-icon/cart-icon.component";
 
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 
@@ -30,6 +31,7 @@ const Header = ({ currentUser }) => (
           SIGN IN
         </Link>
       )}
+      <CartIcon />
     </div>
   </div>
 );

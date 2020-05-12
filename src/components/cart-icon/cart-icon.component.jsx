c7a34cd7bd87035f4674911e18701593409095ce
Dan <danko56666@gmail.com>
2020-05-12 01:35:03 -0400

    alternative to mapdispatchtoprops

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -19,3 +19,30 @@ const mapDispatchToProps = (dispatch) => ({
 });
 
 export default connect(null, mapDispatchToProps)(CartIcon);
+
+/* 
+My take on mapdispatchtoprops. This is entirely optional, if you prefer the way Yihua does it, stick to that. Also
+I am not making any claims on which way is better, but I prefer the way I'm about to show.
+
+Having an explicit mapdispatchtoprops is not necessary. You can just pass in an object in the 2nd argument of connect
+with the actions that you want to use, and use it directly in the component and normal.
+
+---- Code below ----
+import React from "react";
+import { connect } from "react-redux";
+
+import { toggleCartHidden } from "../../redux/cart/cart.actions";
+
+import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
+
+import "./cart-icon.styles.scss";
+
+const CartIcon = ({ toggleCartHidden }) => (
+  <div className="cart-icon" onClick={toggleCartHidden}>
+    <ShoppingIcon className="shopping-icon" />
+    <span className="item-count">0</span>
+  </div>
+);
+
+export default connect(null, { toggleCartHidden })(CartIcon);
+*/

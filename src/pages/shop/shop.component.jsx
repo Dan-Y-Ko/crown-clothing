8a1c24f533c3017e8c3c55ecd9739b26ac7fe006
Dan <danko56666@gmail.com>
2020-05-15 18:32:05 -0400

    lecture 172

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -28,13 +28,17 @@ class ShopPage extends React.Component {
     const { updateCollections } = this.props;
     const collectionRef = firestore.collection("collections");
 
-    this.unsubscribeFromSnapshot = collectionRef.onSnapshot(
-      async (snapshot) => {
-        const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-        updateCollections(collectionsMap);
-        this.setState({ loading: false });
-      }
-    );
+    /* fetch(
+      "https://firestore.googleapis.com/v1/projects/crwn-db-e4d30/databases/(default)/documents/collections"
+    )
+      .then((response) => response.json())
+      .then((collections) => console.log(collections)); */
+
+    collectionRef.get().then((snapshot) => {
+      const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+      updateCollections(collectionsMap);
+      this.setState({ loading: false });
+    });
   }
 
   render() {
